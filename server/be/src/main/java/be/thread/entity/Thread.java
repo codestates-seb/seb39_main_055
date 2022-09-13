2bb9515c97c12575725adbbb855b9430a9e7ea83
upqnu <upgrdrqnu@gmail.com>
2022-09-13 17:45:35 +0900

    #27 feat: entity 매핑

---
+++ server/be/src/main/java/be/thread/entity/Thread.java
@@ -0,0 +1,47 @@
+package be.thread.entity;
+
+import be.audit.BaseEntity;
+import be.user.entity.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "THREAD")
+public class Thread extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long threadId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, name = "STATUS")
+    private ThreadStatus threadStatus = ThreadStatus.THREAD_EXIST;
+
+    @Column(nullable = false)
+    private String body;
+
+    @Column(nullable = false)
+    private int likes;
+
+    @ManyToOne
+    @JoinColumn(name = "USER_ID")
+    private User user;
+
+    public enum ThreadStatus {
+        THREAD_EXIST("존재하는 글"),
+        THREAD_NOT_EXIST("존재하지 않는 글");
+
+        @Getter
+        private String status;
+
+        ThreadStatus(String status) {
+            this.status = status;
+        }
+    }
+}
