1a25f0dbe4ca164d7c4f80225dc1be1271a15b01
MoSangBin <mosangbin94@gmail.com>
2022-09-17 19:53:40 +0900

    refactor: hotplace animation 수정

--- client/src/components/Main/HotPlace/style.ts
+++ client/src/components/Main/HotPlace/style.ts
@@ -49,11 +49,14 @@ export const SImgContainer = styled.div`
   }
 
   @keyframes fadein {
-    from {
+    0% {
       opacity: 0;
     }
-    to {
+    50% {
       opacity: 1;
+    }
+    100% {
+      opacity: 0;
     }
   }
 `;
