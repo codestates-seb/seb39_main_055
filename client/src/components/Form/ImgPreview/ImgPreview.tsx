6146248ce70e2609e093fd908446538ed36f6372
MoSangBin <mosangbin94@gmail.com>
2022-09-19 17:20:18 +0900

    feat: ImgPreview component 추가

---
+++ client/src/components/Form/ImgPreview/ImgPreview.tsx
@@ -0,0 +1,77 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import styled from "styled-components";
+
+export const SPreview = styled.div`
+  width: 200px;
+  height: 200px;
+
+  & > img {
+    width: 200px;
+    height: 200px;
+    object-fit: cover;
+  }
+`;
+
+export const SContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+
+  & > label {
+    display: flex;
+    align-items: center;
+    gap: 7px;
+    cursor: pointer;
+
+    & > svg {
+      width: 20px;
+      height: 20px;
+    }
+  }
+
+  & > input {
+    display: none;
+  }
+`;
+
+interface Prop {
+  id: string;
+  label: string;
+  imgUrl: string;
+  setImgUrl: React.Dispatch<React.SetStateAction<string | ArrayBuffer | null>>;
+}
+
+const ImgPreview = ({ id, label, imgUrl, setImgUrl }: Prop) => {
+  const encodeFileToBase64 = (fileBlob: Blob) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(fileBlob);
+    return new Promise<void>((resolve) => {
+      reader.onload = () => {
+        setImgUrl(reader.result);
+        resolve();
+      };
+    });
+  };
+
+  return (
+    <SContainer>
+      <SPreview>
+        <img src={imgUrl as string} alt="preview-img" />
+      </SPreview>
+      <label htmlFor={id}>
+        {label} <MdOutlineKeyboardArrowRight />
+      </label>
+      <input
+        type="file"
+        id={id}
+        onChange={(e) => {
+          encodeFileToBase64((e.target.files as FileList)[0]);
+        }}
+      />
+    </SContainer>
+  );
+};
+
+export default ImgPreview;
--- client/src/components/Form/index.ts
+++ client/src/components/Form/index.ts
@@ -1,4 +1,5 @@
 export { default as Button } from "./Button/DefaultButton";
 export { ButtonOrange, ButtonWhite } from "./Button/Template";
 export { default as Checkbox } from "./Checkbox/Checkbox";
+export { default as ImgPreview } from "./ImgPreview/ImgPreview";
 export { default as Input } from "./Input/Input";
