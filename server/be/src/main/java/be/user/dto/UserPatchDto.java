65a548cb2a97236d086362f391780835f54fecee
soyoung96 <psy411301@naver.com>
2022-09-19 20:57:17 +0900

    #95 회원정보 수정 API 구현

---
+++ server/be/src/main/java/be/user/dto/UserPatchDto.java
@@ -0,0 +1,25 @@
+package be.user.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Getter
+public class UserPatchDto {
+
+    //유저수정
+    private String nickname;
+
+    @Email(message = "이메일 형식이 맞는 지 확인해주세요")
+    private String email;
+
+    private Double longitude;
+
+    private Double latitude;
+
+    private String image;
+
+}
