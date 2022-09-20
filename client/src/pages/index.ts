2fdd720f296c9adc6577a1e3a92007354cc13dc5
ksheer506 <ksheer506@naver.com>
2022-09-20 21:38:24 +0900

    #91 chore: 파일명 변경

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -1,6 +1,6 @@
 export { default as Main } from "./Main/Main";
 export { default as Mypage } from "./Mypage/Mypage";
 export { default as RegisterPlace } from "./Place/RegisterPlace/RegisterPlace";
-export { default as AddNewPost } from "./Posts/AddNewPost";
+export { default as NewPost } from "./Posts/NewPost";
 export { default as Login } from "./Register/Login/Login";
 export { default as Signup } from "./Register/Signup/Signup";
