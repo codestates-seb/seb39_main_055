82b89033fb1a14012bb9845f5d5dd7a85a4083d9
soyoung96 <psy411301@naver.com>
2022-09-22 00:41:28 +0900

    #123 하트누른 가게 목록 리스트 가져오기 API

--- server/be/src/main/java/be/heart/repository/HeartRepository.java
+++ server/be/src/main/java/be/heart/repository/HeartRepository.java
@@ -3,11 +3,17 @@ package be.heart.repository;
 import be.heart.entity.Heart;
 import be.store.entity.Store;
 import be.user.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface HeartRepository extends JpaRepository<Heart, Long> {
 
     Optional<Heart> findByUserAndStoreAndHeartStatus(User user, Store store, Heart.HeartStatus heartStatus);
+
+    Page<Heart> findByUserAndHeartStatus(Pageable pageable, User user, Heart.HeartStatus heartStatus);
 }
