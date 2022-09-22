0a906b82524365c172a0b0ee5e5b640725382db8
MoSangBin <mosangbin94@gmail.com>
2022-09-22 20:46:35 +0900

    refactor: searchbar 컴포넌트 분리

---
+++ client/src/components/Main/SearchBar/SearchBar.tsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+import search from "../../../assets/icons/search.png";
+
+export const SSearchBar = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 20px;
+  margin: 0 auto;
+  width: 75%;
+  height: 64px;
+  border: 1px solid #707070;
+  border-radius: 40px;
+
+  & > input {
+    outline: none;
+    border: none;
+    width: 90%;
+  }
+
+  & > img {
+    width: 24px;
+    height: 24px;
+    cursor: pointer;
+  }
+`;
+
+const SearchBar = () => {
+  const navigate = useNavigate();
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleInputKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter" && inputValue) {
+      navigate(`/search?search=${inputValue}`);
+      setInputValue("");
+    }
+  };
+
+  const handleSearchIconClick = () => {
+    if (inputValue) navigate(`/search?search=${inputValue}`);
+  };
+
+  return (
+    <SSearchBar>
+      <input
+        value={inputValue}
+        onChange={handleInputChange}
+        onKeyPress={handleInputKeyPress}
+      />
+      <img src={search} alt="search" onClick={handleSearchIconClick} />
+    </SSearchBar>
+  );
+};
+
+export default SearchBar;
