e503f8cfe863b417267572f951ef2f42cc557797
MoSangBin <mosangbin94@gmail.com>
2022-09-24 17:25:42 +0900

    refactor: ReplyCard 컴포넌트 스타일 파일 분리

--- client/src/pages/Posts/PostDetail/ReplyCard/ReplyCard.tsx
+++ client/src/pages/Posts/PostDetail/ReplyCard/ReplyCard.tsx
@@ -1,84 +1,8 @@
 import { useState } from "react";
 import { HiOutlineHeart } from "react-icons/hi";
-import styled from "styled-components";
 
 import { Dots } from "../../../../components";
-
-export const SList = styled.li`
-  border-bottom: 1px solid #bdbdbd;
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  min-height: 150px;
-  padding-bottom: 40px;
-`;
-
-export const SUtils = styled.section`
-  flex-basis: 35px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-
-  svg {
-    font-size: 30px;
-    color: #a5a5a5;
-    cursor: pointer;
-    transition: all 0.3s;
-
-    &:hover {
-      color: #161616;
-    }
-  }
-`;
-
-export const SUserInfo = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 15px;
-
-  & > img {
-    width: 30px;
-    height: 30px;
-    border-radius: 50%;
-    object-fit: cover;
-  }
-
-  & > span {
-    font-size: 16px;
-    color: #161616;
-  }
-`;
-
-export const SBody = styled.p`
-  flex-grow: 1;
-  line-height: 35px;
-`;
-
-export const SBottom = styled.section<{ isLike: boolean }>`
-  flex-basis: 15px;
-  display: flex;
-  align-items: center;
-  gap: 17px;
-  color: #a5a5a5;
-  font-size: 14px;
-
-  & > div {
-    display: flex;
-    align-items: center;
-    cursor: pointer;
-
-    & > svg {
-      margin-right: 2px;
-      font-size: 15px;
-      color: ${({ isLike }) => isLike && "red"};
-      fill: ${({ isLike }) => isLike && "red"};
-    }
-
-    & > span {
-      padding-top: 2px;
-    }
-  }
-`;
+import { SBody, SBottom, SList, SUserInfo, SUtils } from "./style";
 
 interface Prop {
   replyId: string;
