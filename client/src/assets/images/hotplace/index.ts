276d85079ffdb91de6bc75da5d9e91d53e7c694e
MoSangBin <mosangbin94@gmail.com>
2022-09-26 22:25:14 +0900

    fix: 이미지 더미데이터 수정

---
+++ client/src/assets/images/hotplace/index.ts
@@ -0,0 +1,6 @@
+export { default as beauty } from "./beauty.jpg";
+export { default as cafe } from "./cafe.jpg";
+export { default as hospital } from "./hospital.jpg";
+export { default as hotpension } from "./pension.jpg";
+export { default as playground } from "./playground.jpg";
+export { default as restaurant } from "./restaurant.jpg";
