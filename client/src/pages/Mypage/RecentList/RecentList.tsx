30bf799fb0444639ade2c4626e7b561c76e42ab4
so191925 <so191925@gmail.com>
2022-09-26 12:25:02 +0900

    #104 style: UI 구현

--- client/src/pages/Mypage/RecentList/RecentList.tsx
+++ client/src/pages/Mypage/RecentList/RecentList.tsx
@@ -1,22 +1,40 @@
 /* eslint-disable array-callback-return */
 /* eslint-disable consistent-return */
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-import love from "../../../assets/images/mypage/love.png";
+import { mobile, tablet } from "../../../assets";
+import love from "../../../assets/icons/love.png";
 import user from "../../../assets/images/mypage/user.png";
+import defaultImg from "../defaultImg";
 import { recentPlace } from "./RecentDummyData";
 
 const SContainer = styled.div`
   font-family: "ONE-Mobile-Regular";
   display: flex;
+  overflow-x: hidden;
   flex-direction: column;
   width: 100%;
+
   height: auto;
   padding: 20px 40px 20px 40px;
+
+  @media (max-width: 900px) {
+    justify-content: center;
+    height: auto;
+    width: 500px;
+  }
+
+  ${mobile(css`
+    justify-content: center;
+    width: 270px;
+    height: auto;
+  `)}
 `;
 
 const SItemContainer = styled.div`
+  width: 690px;
   display: flex;
+  overflow-x: scroll;
 `;
 
 const SHeader = styled.div`
@@ -36,7 +54,7 @@ const SHeader = styled.div`
   }
 `;
 
-const SInfoContainer = styled.div`
+const SCardContainer = styled.div`
   max-width: 100%;
   display: flex;
   flex-direction: row;
@@ -44,26 +62,32 @@ const SInfoContainer = styled.div`
   height: auto;
 `;
 
-const SImageContainer = styled.div`
-  margin: 10px;
+const SCard = styled.div`
+  width: 220px;
+  height: 315px;
+  margin: 5px;
+  padding: 10px;
+  border: 1px solid ${({ theme }) => theme.colors.black050};
 
   & > img {
-    object-fit:cover
+    object-fit: cover;
     justify-content: space-between;
     width: 100%;
-    height: auto%;
+    height: 68%;
     flex-direction: column;
   }
 `;
 
 const STextInfo = styled.div`
+  padding-top: 10px;
   justify-content: space-between;
   display: flex;
   flex-direction: column;
-  gap: 2px;
+  gap: 4px;
 `;
 
 const STitle = styled.div`
+  font-weight: bold;
   font-size: 14px;
   color: ${({ theme }) => theme.colors.orange500};
 `;
@@ -91,8 +115,8 @@ const RecentList = () => {
       </SHeader>
       <SItemContainer>
         {localstorageData.map((place: any) => (
-          <SInfoContainer key={place.storeId}>
-            <SImageContainer>
+          <SCardContainer key={place.storeId}>
+            <SCard>
               <img
                 src={
                   place.storeImages.length >= 1
@@ -104,11 +128,11 @@ const RecentList = () => {
               />
               <STextInfo>
                 <STitle>{place.category}</STitle>
-                <SArea>{place.adressName}</SArea>
+                <SArea>{place.addressName}</SArea>
                 <SText>{place.storeName}</SText>
               </STextInfo>
-            </SImageContainer>
-          </SInfoContainer>
+            </SCard>
+          </SCardContainer>
         ))}
       </SItemContainer>
     </SContainer>
