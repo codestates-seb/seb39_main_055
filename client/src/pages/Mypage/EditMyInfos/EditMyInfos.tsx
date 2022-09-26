cd63487207c93486878e6c5626fc9efcf6e7e311
ksheer506 <ksheer506@naver.com>
2022-09-26 20:32:14 +0900

    #182 feat: 유저 정보 수정 페이지 완성

--- client/src/pages/Mypage/EditMyInfos/EditMyInfos.tsx
+++ client/src/pages/Mypage/EditMyInfos/EditMyInfos.tsx
@@ -1,131 +1,35 @@
-import { ChangeEvent, useState } from "react";
-import { Navigate } from "react-router-dom";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
-import styled from "styled-components";
 
-import { useCoordinateConvert } from "../../../apis";
-import { colors } from "../../../assets";
+import { getCoordinate, useCoordinateConvert } from "../../../apis";
+import useEditProfiles from "../../../apis/user/editProfiles";
 import {
-  ButtonOrange,
-  Input,
-  InteractiveImage,
-  SearchAddress,
-} from "../../../components";
-import {
+  changeUserAddress,
   changeUserImage,
+  changeUserNickname,
   selectUserInfos,
   useAppDispatch,
   useAppSelector,
 } from "../../../redux";
-import { extractImageInfos } from "../../../utils";
-
-const SForm = styled.form`
-  display: flex;
-  flex-flow: column nowrap;
-  justify-content: center;
-  align-items: center;
-  margin: 100px 0px;
-`;
-
-const SH1 = styled.h1`
-  font-size: 42px;
-  font-weight: normal;
-  margin-bottom: 30px;
-`;
-
-const SaBox = styled.div`
-  display: flex;
-  flex-flow: row nowrap;
-  max-width: 650px;
-  column-gap: 15px;
-`;
-
-const SbBox = styled.div`
-  display: flex;
-  flex-flow: column nowrap;
-  justify-content: center;
-  align-items: center;
-  border: 1px solid ${colors("black050")};
-  border-radius: 13px;
-  padding: 50px 20px;
-`;
-
-const SaSection = styled.section`
-  display: flex;
-  flex-flow: column wrap;
-  justify-content: center;
-  align-items: center;
-  height: 280px;
-  width: 150px;
-`;
-
-const ProfileImage = styled(InteractiveImage)`
-  border-radius: 5px;
-  height: 100px;
-`;
-
-const SFileInput = styled.input`
-  width: 0;
-  height: 0;
-`;
-
-const SP = styled.p`
-  font-weight: bold;
-  margin-top: 10px;
-`;
-
-const SbSection = styled.section`
-  display: flex;
-  flex-flow: column nowrap;
-  justify-content: center;
-  row-gap: 20px;
-`;
-
-const EditInput = styled(Input)`
-  column-gap: 15px;
-
-  & div {
-    flex: 1 1 auto;
-    border-bottom: 1px solid ${colors("black050")};
-  }
-
-  & label {
-    font-weight: bold;
-    flex: 0 1 max-content;
-    width: max-content;
-  }
-
-  & input {
-    height: 35px;
-    font-size: 15px;
-  }
-`;
-
-const SAddressBox = styled.div`
-  display: flex;
-  position: relative;
-`;
-
-const SAddressInput = styled(EditInput)`
-  & input {
-    padding-right: 70px;
-  }
-`;
-
-const SearchAddressButton = styled(SearchAddress)`
-  position: absolute;
-  right: 0;
-  top: 50%;
-  transform: translate(0, -50%);
-  height: 25px;
-  width: 60px;
-  padding: 5px 3px;
-`;
-
-const SButton = styled(ButtonOrange)`
-  /* height: 40px; */
-  margin-top: 40px;
-`;
+import { ThreadImages } from "../../../types";
+import { extractImageInfos, nickNameValidation } from "../../../utils";
+import {
+  EditInput,
+  ProfileImage,
+  SaBox,
+  SAddressBox,
+  SAddressInput,
+  SaSection,
+  SbBox,
+  SbSection,
+  SButton,
+  SearchAddressButton,
+  SFileInput,
+  SForm,
+  SH1,
+  SP,
+} from "./style";
 
 const initialData = {
   nickname: "",
@@ -135,40 +39,86 @@ const initialData = {
 };
 
 const EditMyInfos = () => {
+  const navigate = useNavigate();
   const dispatch = useAppDispatch();
   const { nickname, image, latitude, longitude } =
     useAppSelector(selectUserInfos) || initialData;
-  const { address, isLoading } = useCoordinateConvert({
+  const { address } = useCoordinateConvert({
     x: longitude,
     y: latitude,
   });
-  const [newAddress, setNewAddress] = useState("");
+  const newImage = useRef<ThreadImages | string>(image);
   const [imageErr, setImageErr] = useState(false);
   const [nicknameErr, setNicknameErr] = useState(false);
   const [addressErr, setAddressErr] = useState(false);
+  const { mutate, isLoading, isSuccess } = useEditProfiles();
 
   /* if (!nickname) {
     toast.info("로그인 해주세요.");
 
     return <Navigate to="/login" />;
   } */
+
+  const handleNicknameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const newName = e.target.value;
+
+    if (!nickNameValidation(newName)) {
+      setNicknameErr(true);
+      return;
+    }
+    setNicknameErr(false);
+    dispatch(changeUserNickname(newName));
+  };
+
   const handleImageChange = async (e: ChangeEvent<HTMLInputElement>) => {
     const image = e.target.files;
     if (!image?.length) return;
 
-    const { uri } = (await extractImageInfos([...image]))[0];
-    dispatch(changeUserImage(uri));
+    const imageInfos = (await extractImageInfos([...image]))[0];
+
+    newImage.current = imageInfos;
+    dispatch(changeUserImage(imageInfos.uri));
   };
 
-  const handleAddressChange = () => {};
+  const handleAddressChange = async (address: string) => {
+    const addressName = address.match(/(.*)(?=\s\()/g);
 
-  const handleSubmit = () => {};
+    if (!addressName) {
+      setAddressErr(true);
+      return;
+    }
+    setAddressErr(false);
+    const { documents } = await getCoordinate(addressName[0]);
+
+    if (documents.length === 1) {
+      const { x, y } = documents[0];
+      const newAddress = { longitude: Number(x), latitude: Number(y) };
+
+      dispatch(changeUserAddress(newAddress));
+    }
+  };
+
+  const handleSubmit = () => {
+    const isSubmitReady = !imageErr && !nicknameErr && !addressErr;
+
+    if (!isSubmitReady) {
+      toast.error("입력 필드를 다시 확인해주세요.");
+      return;
+    }
+    mutate({ nickname, latitude, longitude, image: newImage.current });
+  };
+
+  useEffect(() => {
+    if (!isSuccess) return;
+
+    navigate("/mypage");
+  }, [isSuccess, navigate]);
 
   return (
-    <SForm>
+    <SForm onSubmit={(e) => e.preventDefault()}>
       <SH1>회원 정보 수정</SH1>
-      <SbBox>
-        <SaBox>
+      <SaBox>
+        <SbBox>
           <SaSection>
             <ProfileImage
               imageURL={image}
@@ -192,8 +142,8 @@ const EditMyInfos = () => {
               value={nickname}
               placeholder="변경할 이름 입력"
               isError={nicknameErr}
-              errorMsg="dsf"
-              onChange={() => console.log("F")}
+              errorMsg="닉네임은 두 글자 이상 입력해주세요."
+              onChange={handleNicknameChange}
             />
             <SAddressBox>
               <SAddressInput
@@ -203,19 +153,23 @@ const EditMyInfos = () => {
                 value={address}
                 placeholder="변경할 주소 입력"
                 isError={false}
-                errorMsg="dsf"
+                errorMsg="주소를 입력해주세요."
                 readOnly
-                onChange={handleAddressChange}
+                onChange={() => {
+                  "d";
+                }}
               />
               <SearchAddressButton
-                setValue={setNewAddress}
+                setValue={handleAddressChange}
                 setError={setAddressErr}
               />
             </SAddressBox>
           </SbSection>
-        </SaBox>
-        <SButton onClick={handleSubmit}>회원정보 수정</SButton>
-      </SbBox>
+        </SbBox>
+        <SButton onClick={handleSubmit} isPending={isLoading}>
+          회원정보 수정
+        </SButton>
+      </SaBox>
     </SForm>
   );
 };
