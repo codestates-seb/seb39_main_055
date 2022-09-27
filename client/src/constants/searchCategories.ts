0dc9319a1bf4b169fd7ce6a7c8a68b0555bb2c91
ksheer506 <ksheer506@naver.com>
2022-09-27 16:09:48 +0900

    #193 feat: 검색 화면 메뉴 목록 추가

--- client/src/constants/searchCategories.ts
+++ client/src/constants/searchCategories.ts
@@ -7,11 +7,46 @@ import playground from "../assets/images/main-menu/5.png";
 import hospital from "../assets/images/main-menu/6.png";
 
 export const searchCategories = [
-  { img: all, alt: "전체", menuText: "전체", link: "/" },
-  { img: room, alt: "숙소", menuText: "숙소", link: "/" },
-  { img: barber, alt: "미용", menuText: "미용", link: "/" },
-  { img: cafe, alt: "카페", menuText: "카페", link: "/" },
-  { img: restaurant, alt: "맛집", menuText: "맛집", link: "/" },
-  { img: playground, alt: "운동장", menuText: "운동장", link: "/" },
-  { img: hospital, alt: "동물병원", menuText: "동물병원", link: "/" },
+  {
+    img: all,
+    alt: "전체",
+    menuText: "전체",
+    link: "all",
+  },
+  {
+    img: room,
+    alt: "숙소",
+    menuText: "숙소",
+    link: "room",
+  },
+  {
+    img: barber,
+    alt: "미용",
+    menuText: "미용",
+    link: "barber",
+  },
+  {
+    img: cafe,
+    alt: "카페",
+    menuText: "카페",
+    link: "cafe",
+  },
+  {
+    img: restaurant,
+    alt: "맛집",
+    menuText: "맛집",
+    link: "restaurant",
+  },
+  {
+    img: playground,
+    alt: "운동장",
+    menuText: "운동장",
+    link: "playground",
+  },
+  {
+    img: hospital,
+    alt: "동물병원",
+    menuText: "동물병원",
+    link: "hospital",
+  },
 ];
