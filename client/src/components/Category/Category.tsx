aa6cdea122b9d744ecf2cec2e189a81641cc7663
ksheer506 <ksheer506@naver.com>
2022-09-27 16:08:27 +0900

    feat: Category 컴포넌트 쿼리 스트링 Props 추가

--- client/src/components/Category/Category.tsx
+++ client/src/components/Category/Category.tsx
@@ -1,3 +1,5 @@
+import { useSearchParams } from "react-router-dom";
+
 import MenuIcon from "./MenuIcon";
 import { SCategoryMenu } from "./style";
 
@@ -6,20 +8,33 @@ interface CategoryList {
   alt: string;
   menuText: string;
   link: string;
+  selected?: boolean;
 }
 
 interface CategoryProps {
   menuList: CategoryList[];
+  baseQueryString?: string;
+  extraQueryString?: string;
 }
 
-const Category = ({ menuList }: CategoryProps) => {
+const Category = ({
+  menuList,
+  baseQueryString,
+  extraQueryString,
+}: CategoryProps) => {
+  const [params] = useSearchParams();
+
+  const baseQuery = baseQueryString ? `${baseQueryString}&` : "";
+  const extraQuery = extraQueryString ? `${extraQueryString}=` : "";
+
   return (
     <SCategoryMenu>
       {menuList.map(({ menuText, link, img, alt }) => (
         <MenuIcon
           menuText={menuText}
-          link={link}
+          link={`${baseQuery}${extraQuery}${link}`}
           img={img}
+          selected={params.get(extraQueryString || "") === link}
           alt={alt}
           key={menuText}
         />
