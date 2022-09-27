68acc0c42dbbc8c84a968438b0f3f03c8f9586d1
MoSangBin <mosangbin94@gmail.com>
2022-09-27 14:08:01 +0900

    feat: 별점 평균 계산 함수 추가

---
+++ client/src/utils/averageStar.ts
@@ -0,0 +1,10 @@
+import { Review } from "../types";
+
+type AverageStar = (reviewList: Review[]) => string;
+
+export const averageStar: AverageStar = (reviewList) => {
+  const stars = reviewList.map((review) => review.score);
+  const average = stars.reduce((acc, cur) => acc + cur, 0) / reviewList.length;
+
+  return average.toFixed(1);
+};
