1112ecefba8f76b2172cfb2da439bd145d753e07
soyoung96 <psy411301@naver.com>
2022-09-28 00:37:28 +0900

    client삭제

--- client/src/components/Main/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as Category } from "./Category/Category";
-export { default as Header } from "./Header/Header";
-export { default as HotPlace } from "./HotPlace/HotPlace";
-export { default as Pick } from "./Pick/Pick";
-export { default as Recommend } from "./Recommend/Recommend";
-export { default as Review } from "./Review/Review";
