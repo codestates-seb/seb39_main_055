2b7e3923f3f89f6d0cb247a2dc45cb9280a24991
ksheer506 <ksheer506@naver.com>
2022-09-27 22:07:05 +0900

    feat: 압축 미지원 포맷 필터링

--- client/src/utils/imageLoad.worker.ts
+++ client/src/utils/imageLoad.worker.ts
@@ -2,28 +2,29 @@
 import { extractImageInfos } from "./imageProcessor";
 
 const MAX_WIDTH = 1000;
+const incompatible = ["svg+xml", "gif"];
 
-self.addEventListener("message", async ({ data }) => {
+self.addEventListener("message", async ({ data }: MessageEvent) => {
   const { images, canvas } = data;
   const ctx = canvas.getContext("2d");
 
   const compressed = await Promise.all(
     images.map(async (img: File) => {
       // 압축 미지원 포맷 - SVG, GIF
-      if (images.length > 0) {
-        const image = await createImageBitmap(img);
-        const { width, height } = image;
-        const ratio = height / width;
+      const extension = img.type.split("/")[1];
+      if (incompatible.includes(extension)) return img;
 
-        const W = width > MAX_WIDTH ? MAX_WIDTH : width;
-        const H = W * ratio;
+      const image = await createImageBitmap(img);
+      const { width, height } = image;
+      const ratio = height / width;
 
-        canvas.width = W;
-        canvas.height = H;
-        ctx.drawImage(image, 0, 0, W, H);
-        return canvas.convertToBlob({ quality: 0.8 });
-      }
-      return img;
+      const W = width > MAX_WIDTH ? MAX_WIDTH : width;
+      const H = W * ratio;
+
+      canvas.width = W;
+      canvas.height = H;
+      ctx.drawImage(image, 0, 0, W, H);
+      return canvas.convertToBlob({ quality: 0.8 });
     })
   );
 
