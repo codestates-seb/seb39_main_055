9059da6a2359835636f55049ee41d86f5d1b4cbf
MoSangBin <mosangbin94@gmail.com>
2022-09-27 19:04:33 +0900

    feat: 좋아요 api 연결

--- client/src/apis/place/index.ts
+++ client/src/apis/place/index.ts
@@ -1,5 +1,6 @@
 export * from "./addPlace";
 export * from "./deletePlace";
 export * from "./editPlace";
+export * from "./hearts";
 export * from "./placeDetail";
 export { default as usePlace } from "./usePlace";
