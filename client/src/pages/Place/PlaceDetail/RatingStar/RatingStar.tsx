bdb3fb197ab67366341c4933ccfd316c766231ad
MoSangBin <mosangbin94@gmail.com>
2022-09-28 10:58:24 +0900

    feat: 별점 컴포넌트 생성

---
+++ client/src/pages/Place/PlaceDetail/RatingStar/RatingStar.tsx
@@ -0,0 +1,49 @@
+/* eslint-disable no-nested-ternary */
+/* eslint-disable react/no-array-index-key */
+import { Dispatch, SetStateAction } from "react";
+import { AiFillStar } from "react-icons/ai";
+import styled from "styled-components";
+
+const SRatingContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 13px 0px;
+
+  .active {
+    color: #ffc109;
+  }
+
+  & > p {
+    color: #707070;
+    margin-left: 10px;
+  }
+`;
+
+const SStar = styled(AiFillStar)`
+  color: #dbdbdb;
+  cursor: pointer;
+  transition: all 0.4s;
+`;
+
+interface Prop {
+  ratingIndex: number;
+  setRatingIndex: Dispatch<SetStateAction<number>>;
+}
+
+const RatingStar = ({ ratingIndex, setRatingIndex }: Prop) => {
+  return (
+    <SRatingContainer>
+      {[1, 2, 3, 4, 5].map((arrayIndex, index) => (
+        <SStar
+          size={30}
+          key={`rating_${index}`}
+          className={arrayIndex <= ratingIndex ? "active" : "inactive"}
+          onClick={() => setRatingIndex(arrayIndex)}
+        />
+      ))}
+      <p>별점을 남겨주세요.</p>
+    </SRatingContainer>
+  );
+};
+
+export default RatingStar;
