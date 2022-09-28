d0b34536474e14ee33ce288143c15f6198f3bdeb
MoSangBin <mosangbin94@gmail.com>
2022-09-28 16:12:06 +0900

    style: error modal 컴포넌트 분리

---
+++ client/src/components/Modal/ErrorModal/ErrorModal.tsx
@@ -0,0 +1,54 @@
+import { MdOutlineError } from "react-icons/md";
+import styled from "styled-components";
+
+import useModal from "../useModal";
+
+export const Scontainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 30px;
+  padding: 30px;
+  font-size: 18px;
+
+  & > svg {
+    fill: #e51313;
+    color: #e51313;
+    font-size: 50px;
+  }
+
+  & > button {
+    width: 30%;
+    padding: 15px 20px;
+    color: #ffffff;
+    background-color: #374761;
+    border: none;
+    border-radius: 10px;
+    transition: all 0.4s;
+
+    &:hover {
+      scale: 1.1;
+    }
+  }
+`;
+
+interface Prop {
+  body: string;
+}
+
+const ErrorModal = ({ body }: Prop) => {
+  const { closeModal } = useModal();
+
+  return (
+    <Scontainer>
+      <MdOutlineError />
+      <p>{body}</p>
+      <button type="button" onClick={closeModal}>
+        확인
+      </button>
+    </Scontainer>
+  );
+};
+
+export default ErrorModal;
--- client/src/components/Modal/index.ts
+++ client/src/components/Modal/index.ts
@@ -1,4 +1,5 @@
 export { default as DeleteModal } from "./DeleteModal/DeleteModal";
+export { default as ErrorModal } from "./ErrorModal/ErrorModal";
 export { default as LoginModal } from "./LoginModal/LoginModal";
 export { ModalCtx as Modal } from "./ModalContext";
 export { default as useModal } from "./useModal";
