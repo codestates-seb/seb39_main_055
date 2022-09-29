f81c62bdc577e45d24fe0913b19bd0e4f4bdefec
MoSangBin <mosangbin94@gmail.com>
2022-09-29 14:39:40 +0900

    refactor: ReviewForm 컴포넌트 스타일 파일 분리

--- client/src/pages/Place/PlaceDetail/ReviewForm/ReviewForm.tsx
+++ client/src/pages/Place/PlaceDetail/ReviewForm/ReviewForm.tsx
@@ -10,76 +10,7 @@ import { ErrorModal, LoginModal } from "../../../../components/Modal";
 import { useAppSelector } from "../../../../redux";
 import { Store } from "../../../../types";
 import RatingStar from "../RatingStar/RatingStar";
-
-export const STextAreaContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
-  padding: 10px 20px;
-  border: 1px solid #dbdbdb;
-  border-radius: 10px;
-
-  & > div:last-child {
-    display: flex;
-    justify-content: space-between;
-  }
-`;
-
-export const STextArea = styled.textarea`
-  width: 100%;
-  min-height: 125px;
-  outline: none;
-  border: none;
-  color: #000000;
-  font-size: 16px;
-  font-family: "Noto Sans KR", sans-serif;
-  resize: none;
-
-  &::placeholder {
-    font-size: 16px;
-    color: #767676;
-  }
-`;
-
-export const SButton = styled.button`
-  padding: 10px 20px;
-  color: #ffffff;
-  background-color: #ffc107;
-  border: none;
-  border-radius: 10px;
-  font-size: 12px;
-  font-family: "ONE-Mobile-Regular";
-  transition: all 0.4s;
-
-  &:hover {
-    scale: 1.1;
-  }
-
-  &:disabled {
-    color: #161616;
-    background-color: #dbdbdb;
-
-    &:hover {
-      scale: 1;
-    }
-  }
-`;
-
-export const SCancelButton = styled.button`
-  padding: 10px 20px;
-  margin-right: 10px;
-  color: #161616;
-  background-color: #dbdbdb;
-  border: none;
-  border-radius: 10px;
-  font-size: 12px;
-  font-family: "ONE-Mobile-Regular";
-  transition: all 0.4s;
-
-  &:hover {
-    scale: 1.1;
-  }
-`;
+import { SButton, SCancelButton, STextArea, STextAreaContainer } from "./style";
 
 interface Prop {
   isEdit: boolean;
---
+++ client/src/pages/Place/PlaceDetail/ReviewForm/style.ts
@@ -0,0 +1,71 @@
+import styled from "styled-components";
+
+export const STextAreaContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  padding: 10px 20px;
+  border: 1px solid #dbdbdb;
+  border-radius: 10px;
+
+  & > div:last-child {
+    display: flex;
+    justify-content: space-between;
+  }
+`;
+
+export const STextArea = styled.textarea`
+  width: 100%;
+  min-height: 125px;
+  outline: none;
+  border: none;
+  color: #000000;
+  font-size: 16px;
+  font-family: "Noto Sans KR", sans-serif;
+  resize: none;
+
+  &::placeholder {
+    font-size: 16px;
+    color: #767676;
+  }
+`;
+
+export const SButton = styled.button`
+  padding: 10px 20px;
+  color: #ffffff;
+  background-color: #ffc107;
+  border: none;
+  border-radius: 10px;
+  font-size: 12px;
+  font-family: "ONE-Mobile-Regular";
+  transition: all 0.4s;
+
+  &:hover {
+    scale: 1.1;
+  }
+
+  &:disabled {
+    color: #161616;
+    background-color: #dbdbdb;
+
+    &:hover {
+      scale: 1;
+    }
+  }
+`;
+
+export const SCancelButton = styled.button`
+  padding: 10px 20px;
+  margin-right: 10px;
+  color: #161616;
+  background-color: #dbdbdb;
+  border: none;
+  border-radius: 10px;
+  font-size: 12px;
+  font-family: "ONE-Mobile-Regular";
+  transition: all 0.4s;
+
+  &:hover {
+    scale: 1.1;
+  }
+`;
