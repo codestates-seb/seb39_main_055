4f1b99c3d45763e0c251a22efdff6f7adcdc8dc5
MoSangBin <mosangbin94@gmail.com>
2022-09-29 11:36:37 +0900

    refactor: 펫 플레이스 상세 페이지 query 커스텀 훅으롭 분리

---
+++ client/src/apis/place/hooks/usePlaceDetail.ts
@@ -0,0 +1,38 @@
+import { useInfiniteQuery, useQuery } from "react-query";
+
+import { getPlaceDetail } from "../placeDetail";
+import { getInfiniteReview } from "../review";
+
+export const usePlaceDetail = (storeId: string) => {
+  const { data: detailData, isLoading } = useQuery(["place", storeId], () =>
+    getPlaceDetail(storeId)
+  );
+
+  const {
+    data: reviewData,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+  } = useInfiniteQuery(
+    ["review", storeId],
+    ({ pageParam = 1 }) => getInfiniteReview(storeId, pageParam),
+    {
+      getNextPageParam: (lastPage) => {
+        const { totalPages } = lastPage.data.reviews.pageInfo;
+        if (lastPage.nextPage <= totalPages) {
+          return lastPage.nextPage;
+        }
+        return undefined;
+      },
+    }
+  );
+
+  return {
+    detailData,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    isLoading,
+    reviewData,
+  };
+};
