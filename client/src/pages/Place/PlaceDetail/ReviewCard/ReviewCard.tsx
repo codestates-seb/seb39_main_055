08d62ba9b988f09fe0892591b93d3a9a3c2f0d44
MoSangBin <mosangbin94@gmail.com>
2022-09-29 14:38:30 +0900

    refactor: ReviewCard 컴포넌트 스타일 파일 분리

--- client/src/pages/Place/PlaceDetail/ReviewCard/ReviewCard.tsx
+++ client/src/pages/Place/PlaceDetail/ReviewCard/ReviewCard.tsx
@@ -3,78 +3,13 @@ import { useState } from "react";
 import { AiFillStar } from "react-icons/ai";
 import { useMutation, useQueryClient } from "react-query";
 import { useParams } from "react-router-dom";
-import styled from "styled-components";
 
 import { deleteReview, editReview } from "../../../../apis/place";
 import { Dots } from "../../../../components";
 import { useAppSelector } from "../../../../redux";
 import { UserInfos } from "../../../../types";
 import ReviewForm from "../ReviewForm/ReviewForm";
-
-export const SReviewList = styled.li`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  gap: 15px;
-  min-height: 200px;
-  padding: 20px;
-  border: 1px solid #dbdbdb;
-  border-radius: 10px;
-`;
-
-export const SUserInfo = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-
-  & > section {
-    display: flex;
-    align-items: center;
-  }
-
-  & > section > img {
-    width: 45px;
-    height: 45px;
-    margin-right: 15px;
-    border-radius: 50%;
-    object-fit: cover;
-  }
-
-  & > section > div {
-    display: flex;
-    flex-direction: column;
-    gap: 5px;
-  }
-`;
-
-export const SStars = styled.div`
-  display: flex;
-  align-items: center;
-
-  & > svg {
-    margin-right: 2px;
-    color: #ffc109;
-  }
-
-  & > p {
-    display: flex;
-    margin-left: 5px;
-    color: #707070;
-    padding-top: 2px;
-    font-size: 12px;
-  }
-`;
-
-export const SBody = styled.div`
-  color: #434343;
-  font-size: 16px;
-  line-height: 30px;
-`;
-
-export const SDate = styled.p`
-  font-size: 14px;
-  color: #707070;
-`;
+import { SBody, SDate, SReviewList, SStars, SUserInfo } from "./style";
 
 interface Prop {
   reviewId: string;
