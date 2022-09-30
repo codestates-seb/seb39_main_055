8cde613c7d38a34cd86e390fa2217217933128e5
ksheer506 <ksheer506@naver.com>
2022-09-30 20:33:36 +0900

    refactor: Category 컴포넌트 로직이 더 명확하도록 수정

--- client/src/components/Category/Category.tsx
+++ client/src/components/Category/Category.tsx
@@ -9,35 +9,76 @@ interface CategoryList {
   img: string;
   alt: string;
   menuText: string;
-  link: string;
+  baseLink?: string;
+  queryKey?: string; // 카테고리를 구분하는 쿼리 스트링 키(key)
+  queryValue?: string; // 카테고리를 구분하는 쿼리 스트링 값(value)
   selected?: boolean;
 }
 
 interface CategoryProps {
   menuList: CategoryList[];
-  baseQueryString?: string;
-  extraQueryString?: string;
+  queryStrings?: { key: string; value: string }[];
+  selectedQKey?: string;
 }
 
-const Category = memo(
-  ({ menuList, baseQueryString, extraQueryString }: CategoryProps) => {
-    const [params] = useSearchParams();
+function combineQueryStrings(
+  fullQString = "",
+  baseLink = "",
+  queryKey = "",
+  queryValue = ""
+) {
+  let seperator = "";
+  // 바로 앞에 붙는 공통 쿼리 스트링은 없지만 개별 카테고리의 쿼리 스트링이 있을 때 (ex. ?category=cafe)
+  if (!fullQString && queryKey) {
+    seperator = "?";
+  }
+  // 바로 앞에 붙는 공통 쿼리 스트링과 개별 카테고리의 쿼리 스트링이 있을 때 (ex. ?search=서울&category=cafe)
+  if (fullQString && queryKey) {
+    seperator = "&";
+  }
 
-    const baseQuery = baseQueryString ? `${baseQueryString}&` : "";
-    const extraQuery = extraQueryString ? `${extraQueryString}=` : "";
+  const lastQString = queryKey ? `${queryKey}=${queryValue}` : "";
+
+  return `${baseLink}${fullQString}${seperator}${lastQString}`;
+}
+
+/**
+ * @param menuList
+ * @param queryStrings 모든 카테고리에서 공통적으로 사용되는 쿼리 스트링 키, 값. (ex. {key: "search", value: "서울" })
+ * @param selectedQKey 카테고리를 구분하는 데 사용되는 쿼리 스트링 키
+ */
+const Category = memo(
+  ({ menuList, queryStrings = [], selectedQKey }: CategoryProps) => {
+    const [params] = useSearchParams();
+    const fullQString = queryStrings.reduce((link, { key, value }, i) => {
+      if (i < 1) return `?${key}=${value}`;
+
+      return `${link}&${key}=${value}`;
+    }, "");
 
     return (
       <SCategoryMenu>
-        {menuList.map(({ menuText, link, img, alt }) => (
-          <MenuIcon
-            menuText={menuText}
-            link={`${baseQuery}${extraQuery}${link}`}
-            img={img}
-            selected={params.get(extraQueryString || "") === link}
-            alt={alt}
-            key={menuText}
-          />
-        ))}
+        {menuList.map(
+          ({ menuText, baseLink, queryKey, queryValue, img, alt }) => {
+            const pathTo = combineQueryStrings(
+              fullQString,
+              baseLink,
+              queryKey,
+              queryValue
+            );
+
+            return (
+              <MenuIcon
+                menuText={menuText}
+                link={pathTo}
+                img={img}
+                selected={params.get(selectedQKey || "") === queryValue}
+                alt={alt}
+                key={menuText}
+              />
+            );
+          }
+        )}
       </SCategoryMenu>
     );
   }
