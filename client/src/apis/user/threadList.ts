3457356f78658a9aa5a3c5717b55cc521e541076
so191925 <so191925@gmail.com>
2022-09-30 21:02:03 +0900

    chore: 조건 수정

--- client/src/apis/user/threadList.ts
+++ client/src/apis/user/threadList.ts
@@ -4,7 +4,7 @@ import { axiosInstance } from "../../utils";
 export const getThreadList = async (): Promise<Thread[]> => {
   const { data } = await axiosInstance.get(
     `v1/user/thread/list?
-    page=?&size=?`,
+    page=1&size=100`,
     { headers: { tokenNeeded: true } }
   );
   return data.data;
