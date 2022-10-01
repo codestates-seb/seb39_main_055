a0a04027e088735ecffd4576c4861318a581754e
ksheer506 <ksheer506@naver.com>
2022-10-01 14:59:48 +0900

    refactor: Modal 컴포넌트 useRef 이용

--- client/src/components/Modal/ModalContext.tsx
+++ client/src/components/Modal/ModalContext.tsx
@@ -6,6 +6,7 @@ import {
   ReactNode,
   useCallback,
   useEffect,
+  useRef,
   useState,
 } from "react";
 
@@ -22,14 +23,6 @@ import {
 export const MainCtx = createContext<MainContext | null>(null);
 export const CustomizeCtx = createContext<CustomizeContext | null>(null);
 export const OpenCtx = createContext<OpenContext | null>(null);
-const customize: CustomizeContext = {
-  setSize: null,
-  setPosition: null,
-};
-const modalCommand: MainContext = {
-  openModal: null,
-  closeModal: null,
-};
 
 export const ModalCtx = ({
   width,
@@ -47,29 +40,37 @@ export const ModalCtx = ({
   const [modalSize, setModalSize] = useState<Size>({ width, height });
   const [modalPosition, setModalPosition] = useState<Position>(position);
   const [content, setContent] = useState<ReactNode>(null);
+  const customize = useRef<CustomizeContext>({
+    setSize: null,
+    setPosition: null,
+  });
+  const modalCommand = useRef<MainContext>({
+    openModal: null,
+    closeModal: null,
+  });
 
   const openModal = useCallback(
     (component: ReactNode) => {
-      console.log("openModal", "mount", mount, "isOpen", isOpen);
       // 현재 context에 모달이 열려있으면 openModal()을 한번 더 실행했을 때 다른 context에서 뜨는 문제 발생
       // 1. 열려있는 상태일 때 닫아주는 코드 추가: delayed unmount 때문에 350ms 이후에 닫아야 함
-      if (mount && isOpen) {
+      /* if (mount && isOpen) {
         setIsOpen(false);
         setMount(false);
 
         setTimeout(() => {
+          console.log("닫기");
           setMount(true);
           setIsOpen(true);
           setContent(component);
         }, 90);
         return;
-      }
+      } */
 
       setMount(true);
       setIsOpen(true);
       setContent(component);
     },
-    [mount]
+    [mount, isOpen]
   );
   const closeModal = useCallback(() => {
     setIsOpen(false);
@@ -87,15 +88,15 @@ export const ModalCtx = ({
     return () => clearTimeout(timerId);
   }, [isOpen, mount]);
 
-  customize.setSize = setModalSize;
-  customize.setPosition = setModalPosition;
+  customize.current.setSize = setModalSize;
+  customize.current.setPosition = setModalPosition;
 
-  modalCommand.openModal = openModal;
-  modalCommand.closeModal = closeModal;
+  modalCommand.current.openModal = openModal;
+  modalCommand.current.closeModal = closeModal;
 
   return (
-    <MainCtx.Provider value={modalCommand}>
-      <CustomizeCtx.Provider value={customize}>
+    <MainCtx.Provider value={modalCommand.current}>
+      <CustomizeCtx.Provider value={customize.current}>
         <OpenCtx.Provider value={{ isOpen, setIsOpen }}>
           {mount && (
             <Modal
