d990a19a486a4d176b49d58459413fc4dddc310f
MoSangBin <mosangbin94@gmail.com>
2022-10-05 17:33:40 +0900

    fix: 매장 등록 페이지 사업자 등록 폼 삭제

--- client/src/pages/Place/NewPlace/NewPlace.tsx
+++ client/src/pages/Place/NewPlace/NewPlace.tsx
@@ -1,19 +1,6 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-
 import { PlaceForm } from "../../../components";
-import { useAppSelector } from "../../../redux";
 
 const NewPlace = () => {
-  const navigate = useNavigate();
-  const { userInfos } = useAppSelector((state) => state.user);
-
-  useEffect(() => {
-    if (userInfos?.userRole !== "ROLE_OWNER") {
-      navigate("/business");
-    }
-  }, [userInfos, navigate]);
-
   return <PlaceForm isEditPage={false} />;
 };
 
