b2657e6fd02560412b512db04cc88c6716e1f858
ksheer506 <ksheer506@naver.com>
2022-10-05 14:38:43 +0900

    #321 feat: 유저 정보 받아올 때 토큰 유효기간 검사 로직 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,7 +5,7 @@ import { useQuery } from "react-query";
 import { Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 
-import { fetchUserInfos } from "./apis/user/login";
+import { fetchUserInfos, renewAccessToken } from "./apis";
 import { Modal, SharedLayout } from "./components";
 import { Main, Oauth } from "./pages";
 import {
@@ -17,63 +17,38 @@ import {
 } from "./redux";
 import { PlaceRouter, PostRouter, SearchRouter, UserRouter } from "./Routers";
 import { TokenErrorResponse, UserInfos } from "./types";
-import { axiosInstance } from "./utils";
-
-const payload =
-  "eyJ1c2VyUm9sZSI6IlJPTEVfVVNFUiIsInVzZXJJZCI6MjcsImVtYWlsIjoia3NoZWVyNTA2QG5hdmVyLmNvbSIsInN1YiI6IjI3IiwiaWF0IjoxNjY0ODYyOTMwLCJleHAiOjE2NjUxMjIxMzB9";
-// console.log(atob(payload));
-// console.log(Math.floor(Date.now() / 1000));
+import { isTokenExpired } from "./utils";
 
 const App = () => {
   const dispatch = useAppDispatch();
-  const { loginStatus, refreshToken } = useAppSelector(selectUser);
+  const { loginStatus, accessToken, refreshToken } = useAppSelector(selectUser);
 
   // 메인 페이지 로드 후 로그인 상태 확인
-  const { error } = useQuery<UserInfos, AxiosError<TokenErrorResponse>>(
+  useQuery<UserInfos, AxiosError<TokenErrorResponse>>(
     ["userInfos", loginStatus],
     fetchUserInfos,
     {
       enabled: loginStatus,
       staleTime: 10 * 60 * 1000, // 10분
       refetchOnWindowFocus: false,
-      refetchInterval: 1 * 60 * 1000,
+      /* refetchInterval: 1 * 60 * 1000, */
       retry: 2,
       onSuccess: (data) => {
         dispatch(initializeUserInfos(data));
       },
       onError: async (err) => {
-        // dispatch(logOutUser());
-        if (err.message === "토큰 기한 만료") {
-          const { data } = await axiosInstance.post("/v1/token-refresh", {
-            refreshToken,
-          });
-          console.log("refresh 토큰", data);
+        if (err.message !== "토큰 기한 만료") {
+          dispatch(logOutUser());
+        }
+      },
+      // 유저 정보 받아오고 나서 매번 토큰 만료 여부 확인
+      onSettled: async () => {
+        if (isTokenExpired(accessToken)) {
+          await renewAccessToken(refreshToken);
         }
       },
     }
   );
-
-  /* useQuery(
-    ["userInfos", "tokenExpired", error],
-    async () => {
-      if (!error) return;
-      if (error.message === "토큰 기한 만료") {
-        const { data } = await axiosInstance.post("/v1/token-refresh");
-      }
-      console.log(error.message);
-    },
-    {
-      enabled: !!error,
-      staleTime: 10 * 1000, // 6시간
-      refetchOnWindowFocus: false,
-      onSuccess: (data) => {
-        "f";
-      },
-      onError: () => {
-        dispatch(logOutUser());
-      },
-    }
-  ); */
 
   return (
     <>
