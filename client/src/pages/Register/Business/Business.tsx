91689322a01a9ef2291794ec074a315a7e7c44be
MoSangBin <mosangbin94@gmail.com>
2022-10-05 10:37:47 +0900

    feat: 네이버, 카카오 소셜 로그인 연결

---
+++ client/src/pages/Register/Business/Business.tsx
@@ -0,0 +1,117 @@
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+
+import { ButtonOrange, CustomCalendar, Input } from "../../../components";
+import { useValidate } from "../../../hooks";
+import { businessNumberValidation, notBlank } from "../../../utils";
+
+export const SContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 100px 0;
+  min-height: calc(100vh - 380px);
+
+  & > h1 {
+    margin-bottom: 64px;
+    color: #161616;
+    font-size: 32px;
+  }
+
+  & > form {
+    display: flex;
+    flex-direction: column;
+    gap: 60px;
+    width: 100%;
+    max-width: 660px;
+    padding: 100px 60px;
+    border: 1px solid #dbdbdb;
+    border-radius: 0 0 10px 10px;
+    font-family: "Noto Sans KR", sans-serif;
+  }
+
+  @media screen and (max-width: ${({ theme }) => theme.breakPoints.tablet}) {
+    & > div {
+      width: 90%;
+    }
+
+    & > form {
+      width: 90%;
+    }
+  }
+
+  @media screen and (max-width: ${({ theme }) => theme.breakPoints.mobile}) {
+    margin: 50px 0;
+
+    & > h1 {
+      font-size: 21px;
+    }
+
+    & > form {
+      padding: 25px;
+    }
+  }
+`;
+
+export const SButtonContainer = styled.section`
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+`;
+
+const Business = () => {
+  const [nameValue, nameError, handleName, checkName] = useValidate(notBlank);
+  const [
+    businessNumberValue,
+    businessNumberError,
+    handleBusinessNumber,
+    checkBusinessNumber,
+  ] = useValidate(businessNumberValidation);
+
+  const [date, setDate] = useState({
+    value: "",
+    isError: false,
+  });
+
+  useEffect(() => {
+    console.log(date.value);
+  }, [date]);
+
+  return (
+    <SContainer>
+      <h1>사업자 등록</h1>
+      <form onSubmit={() => console.log("asd")}>
+        <Input
+          label="대표자 성명"
+          id="대표자 성명"
+          value={nameValue}
+          isError={nameError}
+          errorMsg="두 글자 이상 입력해주세요."
+          placeholder="대표자 성명을 입력해주세요."
+          onChange={(e) => handleName(e)}
+        />
+        <CustomCalendar
+          value={date.value}
+          setValue={setDate}
+          isError={date.isError}
+        />
+        <Input
+          label="사업자 등록 번호"
+          id="사업자 등록 번호"
+          type="tel"
+          value={businessNumberValue}
+          isError={businessNumberError}
+          errorMsg="특수문자를 제외한 10자리 등록 번호를 입력해주세요."
+          placeholder="특수문자를 제외한 10자리 등록 번호를 입력해주세요."
+          onChange={(e) => handleBusinessNumber(e)}
+        />
+        <SButtonContainer>
+          <ButtonOrange>등록</ButtonOrange>
+        </SButtonContainer>
+      </form>
+    </SContainer>
+  );
+};
+
+export default Business;
